cbf88ce24e617bd24df0fcf2967fa1f326d9a46c
james <james.barbosa@gmail.com>
2024-02-20 06:51:07 +0800

    Added cart tem count display

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
+import { useContext } from "react"
+import CartContext from "../store/CartContext"
+
 export default function Header() {
+    let cartContext = useContext(CartContext)
+
+    const totalCount = cartContext.items.reduce((totalCount, item) => {
+        return totalCount + item.quantity;
+    }, 0)
+
     return <div id="header">
         <div className="container">
             <div></div>
             <span className="title">React Toys Shop</span>
             <div className="cart-container">
-                <button>Cart (0)</button>
+                <button>Cart ({totalCount})</button>
             </div>
         </div>
     </div>
