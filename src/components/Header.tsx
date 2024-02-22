2e1830dac5f793c6a2da1dcc52f3fa1a8e77f7cf
james <james.barbosa@gmail.com>
2024-02-22 19:05:06 +0800

    Added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect, useState } from "react"
+import toyLogo from "../assets/images/toy-logo.png";
 // import CartContext from "../store/CartContext"
 // import UserContext from "../store/UserContext"
 import { useDispatch, useSelector } from "react-redux"
@@ -33,11 +34,17 @@ export default function Header() {
 
     return <div id="header">
         <div className="container">
-            <div></div>
-            <span className="title">React Toys Shop</span>
-            {/* <div className="cart-container"> */}
-                <button onClick={() => handleShowCart()} className={`${isBlink ? 'blink' : ''} cart-btn`}>Cart ({totalCount})</button>
-            {/* </div> */}
+            <div className="header-title-container">
+                <img className="header-title-image" src={toyLogo} />
+                <span className="header-title">React Toys Shop</span>
+            </div>
+
+
         </div>
+        <div className="cart-container">
+            <button onClick={() => handleShowCart()} className={`${isBlink ? 'blink' : ''} cart-btn`}>Cart ({totalCount})</button>
+
+        </div>
+
     </div>
 }
