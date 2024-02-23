3b0b7153f1c5d78b3042e2dac023f27d2e230d1d
james <james.barbosa@gmail.com>
2024-02-23 12:29:31 +0800

    Remove notif in shop item retrieval

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -6,22 +6,15 @@ import { notifActions } from "../store";
 export default function Shop() {
     const [shopItems, setShopItems] = useState([]);
     const [isLoading, setIsLoading] = useState(false)
-    const dispatch = useDispatch();
 
     useEffect(() => {
         async function getItems() {
-            dispatch(notifActions.openNotifDisplay({ status: "warning", message: "Loading..." }))
             setIsLoading(prev => true)
             let items = await fetch('http://localhost:3000/items')
             let itemJson = await items.json()
             setIsLoading(prev => false)
-            dispatch(notifActions.openNotifDisplay({ status: "success", message: "Successfully loaded data" }))
-
             setShopItems(prev => [...itemJson.items])
         }
-        setTimeout(() => {
-            dispatch(notifActions.closeNotifDisplay())
-        }, 4000)
         getItems();
     }, [])
 
