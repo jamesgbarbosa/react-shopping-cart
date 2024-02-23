d5ac0e7c636856dd3e404955ec9a86ead0f863d1
james <james.barbosa@gmail.com>
2024-02-23 12:49:56 +0800

    Clean up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { Fragment } from 'react'
 import './App.css'
-import CartModal from './components/CartModal'
-import Header from './components/Header'
 import Shop from './components/Shop/Shop'
-import CheckoutModal from './components/CheckoutModal'
-import Notif from './components/UI/Notif'
-import { useSelector } from 'react-redux'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import MainRoot from './components/MainRoot'
-// import { CartContextProvider } from './store/CartContext'
-// import { UserContextProvider } from './store/UserContext'
 
 const router = createBrowserRouter([
   {
@@ -33,17 +25,6 @@ function App() {
 
   return (
     <RouterProvider router={router}></RouterProvider>
-    // <UserContextProvider>
-    //   <CartContextProvider>
-    // <Fragment>
-    //   <Notif status={notifSelector.status} isHidden={!notifSelector.isOpen} message={notifSelector.message} />
-    //   <Header />
-    //   <Shop />
-    //   <CartModal />
-    //   <CheckoutModal />
-    // </Fragment>
-    /* </ CartContextProvider> */
-    // </UserContextProvider>
   )
 }
 
