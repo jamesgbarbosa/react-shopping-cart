f7b875555b39a7a69352e4c38ee16c800f75ba2d
james <james.barbosa@gmail.com>
2024-02-26 12:11:16 +0800

    Added form inputs on checkoutmodal

--- src/components/CheckoutModal.tsx
+++ src/components/CheckoutModal.tsx
@@ -2,9 +2,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { cartActions, cartModalActions, notifActions, submitCart } from "../store";
 import Modal from "./UI/Modal";
 import { createPortal } from "react-dom";
+import { useRef, useState } from "react";
 
 export default function CheckoutModal() {
     const cartModalSelector = useSelector((state) => state.cartModal.progress)
+    const [formData, setFormData] = useState({
+        name: '',
+        email: ''
+    })
+    const formRef = useRef();
+
     const dispatch = useDispatch();
 
     function onHandleClose() {
@@ -12,12 +19,35 @@ export default function CheckoutModal() {
     }
 
     async function onSubmit() {
-        dispatch(submitCart({cart: "data"}))
+        setFormData( prev => ({}))
+        formRef.current.reset();
+        dispatch(submitCart({ ...formData}))
+    }
+
+    function handleOnChangeInput(event, controlName) {
+        let value = event.target.value;
+        setFormData( prev => {
+            return {...prev, [controlName]: value}
+        })
     }
 
     return createPortal(<Modal onClose={onHandleClose} isOpen={cartModalSelector == 'checkout'}>
         <div className="checkout-modal">
-            <h3>Checkout</h3>
+            <form ref={formRef}>
+                <div className="cart-header">
+                    <h3>Checkout</h3>
+                </div>
+                <section className="checkout-form">
+                    <div className="form-control">
+                        <label>Name: </label>
+                        <input onChange={(e) => handleOnChangeInput(e, 'name')} type="text" id="name" name="name" value={formData?.name} required></input>
+                    </div>
+                    <div className="form-control">
+                        <label>Email: </label>
+                        <input onChange={(e) => handleOnChangeInput(e, 'email')} type="text" id="email" name="email" value={formData?.email}></input>
+                    </div>
+                </section>
+            </form>
             <div className="cart-footer">
                 <div className="cart-modal-buttons-container">
                     <button className="cart-modal-button" onClick={() => onHandleClose()}>Close</button>
